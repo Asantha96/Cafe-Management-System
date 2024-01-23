23b92dd8ae256b0669b9d0c380710978b5cd39e4
Asantha <asanthadhananjaya@gmail.com>
2024-01-23 09:51:35 +0530

    api related to category implemented

---
+++ src/main/java/com/asa/cafe/entity/Category.java
@@ -0,0 +1,33 @@
+package com.asa.cafe.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@NamedQuery(name = "Category.getAllCategory", query = "SELECT c FROM Category c")//hql query
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@DynamicInsert
+@DynamicUpdate
+@Entity
+@Table(name = "category")
+public class Category implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+}
---
+++ src/main/java/com/asa/cafe/rest/CategoryRest.java
@@ -0,0 +1,21 @@
+package com.asa.cafe.rest;
+
+import com.asa.cafe.entity.Category;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RequestMapping("/category")
+public interface CategoryRest {
+
+    @PostMapping("/add")
+    ResponseEntity<String> addNewCategory(@RequestBody(required = true)Map<String, String> requestMap);
+
+    @GetMapping("/get")
+    ResponseEntity<List<Category>> getAllCategory(@RequestParam(required = false) String filterValue);
+
+    @PostMapping("/update")
+    ResponseEntity<String> updateCategory(@RequestBody(required = true) Map<String, String> requestMap);
+}
